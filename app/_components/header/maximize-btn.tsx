4dd310d2b25bc03eb97f2e979beb35c79093d13e
stpn48 <133258815+stpn48@users.noreply.github.com>
2024-11-30 19:04:46 +0100

    Improve fullscreen button

--- app/_components/header/maximize-btn.tsx
+++ app/_components/header/maximize-btn.tsx
@@ -4,19 +4,44 @@ import { Maximize } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 
 export function MaximizeBtn() {
-  const [fullscreenActive, setFullscreenActive] = useState(false);
+  const [fullscreenActive, setFullscreenActive] = useState(!!document.fullscreenElement);
 
   const handleClick = useCallback(() => {
-    setFullscreenActive((prev) => !prev);
+    if (document.fullscreenElement) {
+      document.exitFullscreen().catch(() => console.error("Error disabling fullscreen"));
+    } else {
+      document.documentElement
+        .requestFullscreen()
+        .catch(() => console.error("Error enabling fullscreen"));
+    }
   }, []);
 
   useEffect(() => {
-    if (fullscreenActive) {
-      document.body.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
-  }, [fullscreenActive]);
+    const handleFullscreenChange = () => {
+      setFullscreenActive(!!document.fullscreenElement);
+    };
 
-  return <Maximize onClick={handleClick} className="size-4 cursor-pointer text-foreground" />;
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key.toLowerCase() === "f") {
+        handleClick();
+      }
+    };
+
+    // Listen for changes in fullscreen state
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    window.addEventListener("keypress", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+      window.removeEventListener("keypress", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <Maximize
+      onClick={handleClick}
+      className={`size-4 cursor-pointer text-foreground`}
+      aria-label={fullscreenActive ? "Exit fullscreen" : "Enter fullscreen"}
+    />
+  );
 }
